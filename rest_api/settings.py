f34677c2031454963c271d266b682be4ddce3f02
Nicholas Bates <nbates66@gmail.com>
2021-08-31 11:52:53 -1000

    Created .envrc and moved all secrets. added .envrc to .gitignore

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ytsk4p0kve)h1x#2o*rkhnd*9dkmr%a9eermri-^%^+bn74_(8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
