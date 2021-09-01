05926c9ab1eb8cada202367b5136299ce9fa60ab
Nicholas Bates <nbates66@gmail.com>
2021-09-01 13:33:21 -1000

    Added serialization to handle requests

--- core/views.py
+++ core/views.py
@@ -5,18 +5,21 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .serializers import PostSerializer
+from .models import Post
+
 
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
-        data = {
-            'name': 'Nick',
-            'age': 32
-        }
-        return Response(data)
+        qs = Post.objects.all()
+        post = qs.first()
+        serializer = PostSerializer(qs, many=True)  # return multiple instances
+        # serializer = PostSerializer(post) <- return single instance
+        return Response(serializer.data)
 
-# def test_view(request):
-    # data = {
-    #     'name': 'Nick',
-    #     'age': 32
-    # }
-#     return JsonResponse(data)
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
